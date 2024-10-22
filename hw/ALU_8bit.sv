4ab952b867c72e23d036cabb25d5f5134be91e0a
Thepantsman <87450154+Thepantsman@users.noreply.github.com>
2024-10-22 18:44:23 -0400

    Update ALU_8bit.sv

--- hw/ALU_8bit.sv
+++ hw/ALU_8bit.sv
@@ -1,6 +1,6 @@
 /*
- Author: _______
- Cite:
+ Author: Sepanta Samani
+ Cite: Will Lerman
 */
 
 module ALU_8bit(input wire[7:0] a, input wire[7:0] b, input wire[2:0] op, output reg[7:0] out);
@@ -8,13 +8,13 @@ always @(*) begin
 case(op)
 3'b000: out = a + b;
   // add in your code to make the ALU works as shown
-3'b001:          ; // sub
-3'b010:          ; // and
-3'b011:          ; // or
-3'b100:          ; // xor
-3'b101:          ; // not
-3'b110:          ; // shl
-3'b111: ; // shr
+3'b001: a - b; // sub
+3'b010: a & b; // and
+3'b011: a | b; // or
+3'b100: a ^ b; // xor
+3'b101: ~a; // not
+3'b110: a << b [2:0]; // shl
+3'b111: a >> b [2:0]; // shr
 default: out = 0;
 endcase
 end
